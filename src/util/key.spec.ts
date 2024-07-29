6fc4ea0254b5469eda2d658c6b60817be407c6af
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2024-07-29 01:52:34 -0700

    feat: impl callOpenApi function (#2)
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- src/util/key.spec.ts
+++ src/util/key.spec.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect, beforeEach } from 'vitest';
 import { setApiKey, getApiKey, ApiKeyNotFoundException } from './key';
+import { Micro } from 'effect';
 
 describe('Key Utility Functions', () => {
   beforeEach(() => {
@@ -10,19 +11,20 @@ describe('Key Utility Functions', () => {
   it('should set and get the API key correctly', () => {
     const testKey = 'test-api-key';
     setApiKey(testKey);
-    expect(getApiKey()).toBe(testKey);
-  });
-
-  it('should throw an ApiKeyNotFoundException if the API key is not set', () => {
-    expect(() => getApiKey()).toThrow(ApiKeyNotFoundException);
+    expect(Micro.runSync(getApiKey())).toBe(testKey);
   });
 
   it('should throw an ApiKeyNotFoundException with the correct message if the API key is not set', () => {
-    try {
-      getApiKey();
-    } catch (e) {
-      expect(e).toBeInstanceOf(ApiKeyNotFoundException);
-      expect((e as ApiKeyNotFoundException).message).toContain('API key is not set');
-    }
+    getApiKey().pipe(
+      Micro.match({
+        onSuccess: () => {
+          throw new Error('Should not be called');
+        },
+        onFailure: (e) => {
+          expect(e).toBeInstanceOf(ApiKeyNotFoundException);
+          expect((e as ApiKeyNotFoundException).message).toContain('API key is not set');
+        },
+      })
+    );
   });
 });
--- src/util/key.ts
+++ src/util/key.ts
@@ -1,3 +1,5 @@
+import { Micro } from 'effect';
+
 let key: string | null = null;
 
 export class ApiKeyNotFoundException extends Error {}
@@ -6,14 +8,16 @@ export const setApiKey = (newKey: string) => {
   key = newKey;
 };
 
-export const getApiKey = () => {
+export const getApiKey = (): Micro.Micro<string, ApiKeyNotFoundException> => {
   if (!key) {
-    throw new ApiKeyNotFoundException(
-      `API key is not set.
+    return Micro.fail(
+      new ApiKeyNotFoundException(
+        `API key is not set.
       please call setApiKey(keys)
       if you have not api key, you can issue api key at https://open.assembly.go.kr/portal/openapi/openApiActKeyPage.do?tabIdx=1`
+      )
     );
   }
 
-  return key;
+  return Micro.succeed(key);
 };
