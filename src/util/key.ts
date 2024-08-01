b0c20532a6f475ad52bcb048f6c8bf6c2f0dd7ac
sunrabbit123 <qudwls185@naver.com>
2024-07-31 20:31:24 -0700

    remove: effect dependency
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- src/util/key.ts
+++ src/util/key.ts
@@ -1,23 +1,27 @@
-import { Micro } from 'effect';
-
 let key: string | null = null;
 
 export class ApiKeyNotFoundException extends Error {}
 
+/**
+ * @description API key를 설정합니다.
+ * @param newKey 설정할 API key
+ */
 export const setApiKey = (newKey: string) => {
   key = newKey;
 };
 
-export const getApiKey = (): Micro.Micro<string, ApiKeyNotFoundException> => {
+/**
+ * @throws { ApiKeyNotFoundException } API key가 설정되지 않았을 경우 발생합니다.
+ * @returns { string } 설정된 API key를 반환합니다.
+ */
+export const getApiKey = (): string => {
   if (!key) {
-    return Micro.fail(
-      new ApiKeyNotFoundException(
-        `API key is not set.
+    throw new ApiKeyNotFoundException(
+      `API key is not set.
       please call setApiKey(keys)
       if you have not api key, you can issue api key at https://open.assembly.go.kr/portal/openapi/openApiActKeyPage.do?tabIdx=1`
-      )
     );
   }
 
-  return Micro.succeed(key);
+  return key;
 };
