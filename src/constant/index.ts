06b1de2c0ecf98100757c3e54ac7ecdfa9166523
statpan <statpan@outlook.com>
2024-08-01 18:24:21 +0900

    chore: change some variable names after review

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,5 +1,13 @@
 /**
  * @description if you modify this properties, you should upgrade the minor version because it will be a break changes
+ * @ignore `~명`과 같은 이름을 지칭하는 변수의 경우 `Name`으로 끝나야함, 예외적으로 구분명은 `Division`으로 끝남
+ * @ignore `~코드`와 같은 이름을 지칭하는 변수의 경우 `Code`로 끝나야함
+ * @ignore `~일자`와 같은 이름을 지칭하는 변수의 경우 `Date`로 끝나야함
+ * @ignore `~구분`과 같은 이름을 지칭하는 변수의 경우 `Division`으로 끝나야함, 만약 `구분명`이라면 `Division`을 사용
+ * @ignore `~처리`과 같은 이름을 지칭하는 변수의 경우 `Process`으로 끝나야함
+ * @ignore `~상정`과 같은 이름을 지칭하는 변수의 경우 `Present`으로 끝나야함
+ * @ignore `~회부`과 같은 이름을 지칭하는 변수의 경우 `Submit`으로 끝나야함
+ * @ignore `~의결`과 같은 이름을 지칭하는 변수의 경우 `Resolution`으로 끝나야함
  */
 export const translatedVariableDictionary = {
   국회의원코드: 'lawmakerCode',
@@ -14,7 +22,7 @@ export const translatedVariableDictionary = {
   선거구구분명: 'electoralDistrictDivisionName',
   위원회명: 'committeeName',
   소속위원회명: 'belongingCommitteeName',
-  재선구분명: 're-electionClassificationDivision',
+  재선구분명: 'reElectionClassificationDivision',
   당선대수: 'GTELT_ERACO',
   성별: 'gender',
   전화번호: 'phoneNumber',
@@ -36,15 +44,15 @@ export const translatedVariableDictionary = {
   제안자구분: 'proposerDivision',
   제안일: 'proposeDate',
   소관위코드: 'committeeId',
-  소관위: 'committee',
-  소관위회부일: 'committeeDate',
+  소관위: 'jurisdictionCommittee',
+  소관위회부일: 'jurisdictionCommitteeSubmitDate',
   위원회심사_처리일: 'committeeReviewProcessDate',
   의안상세정보_URL: 'billDetailUrl',
   대표발의자: 'leadProposer',
-  법사위처리결과: 'lawProcessResult',
-  법사위처리일: 'lawProcessdate',
-  법사위상정일: 'lawPresent',
-  법사위회부일: 'lawSubmitDate',
+  법사위처리결과: 'legislationAndJudiciaryCommitteeProcessResult',
+  법사위처리일: 'legislationAndJudiciaryCommitteeProcessDate',
+  법사위상정일: 'legislationAndJudiciaryCommitteePresentDate',
+  법사위회부일: 'legislationAndJudiciaryCommitteeSubmitDate',
   소관위처리결과: 'committeeProcessResult',
   소관위처리일: 'committeeProcessDate',
   소관위상정일: 'committeePresentDate',
